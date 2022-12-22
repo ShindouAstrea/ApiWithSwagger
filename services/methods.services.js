eb7653b713962b252b27928ba9f7c4bc24b0252d
Jsilva_dev <jose.silva.a@mail.pucv.cl>
2022-12-22 17:54:31 -0300

    Fix: methods and get data

--- services/methods.services.js
+++ services/methods.services.js
@@ -1,33 +1,57 @@
 const analize = (data)=>{
     let result = {};
     //Se guardan y procesan fechas de inicio y fin.
-    let startDate = data["fechaCreacion"];
-    let endDate= data["fechaFin"];
-    let fechasCompletas = completar(startDate,endDate);
-    
+    const startDate = data["fechaCreacion"];
+    const endDate= data["fechaFin"];
+    const fechasCompletas = completar(startDate,endDate);
+    result["resultado"]= fechasCompletas;
+        //Validacion del objeto
+
      if(Object.entries(result).length!=0) return result;
     else return "Algo salió mal";
 };
 function completar(startDate,endDate){
     let arreglo =[];
+    const stDate = new Date(startDate);
+    const finDate = new Date(endDate);
     // Separacion del año , mes y dia de la fecha de inicio.
-    let startYear = data["fechaCreacion"].slice(0,4);
-    let startMonth = data["fechaCreacion"].slice(5,7);
-    let startDay = data["fechaCreacion"].slice(8,10);
-    // Separacion del año , mes y dia de la fecha de fin.
-    let endYear = data["fechaFin"].slice(0,4);
-    let endMonth = data["fechaFin"].slice(5,7);
-    let endDay = data["fechaFin"].slice(8,10);
-    
-    let diferenciaYears =  endYear - startYear;
-    let difernciaMonths = endMonth - endMonth ;
-    switch(diferenciaYears){
-        case diferenciaYears > 0:
-                
-            default:
-                
+    const startYear = stDate.getFullYear(startDate);
+    const startMonth = stDate.getMonth(startDate) - 1;
+    console.log("Mes capturado : "+startMonth);
+    const startDay = stDate.getDay(startDate);
 
+    // Separacion del año , mes y dia de la fecha de fin.
+    const endYear = finDate.getFullYear(endDate);
+    const endMonth = finDate.getMonth(endDate) - 1;
+    const endDay = finDate.getDay(endDate);
+    
+    // Inicializacio de los contadores
+    let leftYears =  endYear - startYear;
+    let leftMonths = endMonth - startMonth ;
+    let leftDays = endDay- startDay ;
+
+    // Se inserta la fecha inicial
+    arreglo.push(startDate);
+    switch(leftYears){        
+        default:
+            let newMonth = startMonth+1 ;
+            console.log(newMonth);
+            while(leftMonths>0){
+                const newDate = new Date(startYear,newMonth,startDay);
+                leftMonths --;
+                let year = newDate.getFullYear();
+                let month = newDate.getMonth();
+                // Para javascript el mes 1 es Febrero. Asi que se reduce el contador.
+                month -- ;
+                year = year.toString();
+                month = month.toString();
+                let cero = "0";
+                month < 10 ? true : cero.concat(month);
+                arreglo.push(year.concat("-",month,"-",startDay)) ;
+                newMonth++;
+            }
     };
+    arreglo.push(endDate);
     return arreglo;
 }
 module.exports={
--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 80 ;
+const port = process.env.PORT || 400 ;
 const completeData = require('./../services/methods.services.js') ;
-const testData = require('../dataTest/data.json');
-
 app.use(express.json());
-const resp = {
-    "name": "Este es el nombre",
-    "description": "prueba de JSON",
-    "Array":[
-        {
-            "object1":"objeto1",
-            "name":"nombre"
-        },
-        {
-            "object2":"objeto2",
-            "name":"nombre2"
-        }
-    ]
-}
+
 app.use(cors());
 
 app.post('/test/',(req,res)=>{
     let parametro = req.body;
-    let respuesta = completeData.analize(parametro);
-    res.json(respuesta);
+    console.log(parametro);
+    res.json(completeData.analize(parametro));
 })
 
-app.get('/respuesta',(req,res)=>{
-    res.json(resp) ;
-})
 app.get('/',(req,res)=>{
-    res.send("HOla");
+    res.send("Hola");
 })
 
 app.listen(port,()=> {
